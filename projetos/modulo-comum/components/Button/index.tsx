4ebddb11197052ed6eeafb40e4deb413a17d6e99
edutanaka <edutanakabr@gmail.com>
2021-04-29 10:41:33 -0300

    feat(projet): add initial project

---
+++ projetos/modulo-comum/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ButtonWrapper = styled.button`
+	color: blue,
+`;
+
+export default function Button() {
+	return (
+		<ButtonWrapper>
+			Botao
+		</ButtonWrapper>
+	)
+}
---
+++ projetos/site/pages/index.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import Button from '@edutanaka/modulo-comum/components/Button'
+
+const Title = styled.h1`
+  color: red;
+  font-size: 50px;
+`
+
+export default function Home() {
+  return (
+    <div>
+      <Button />
+      <Title>
+        My page
+      </Title>
+    </div>
+  )
+}
